fe73a8dd80ca6d2e1281216ac4c462db5c1c67b5
ihsanmuhammadi <ihsanmiqbal24@gmail.com>
2023-12-10 08:43:13 +0700

    Login, Register, Main Page (Admin & user)

---
+++ src/martmain/MartMain.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package martmain;
+
+/**
+ *
+ * @author ihsanmiqbal
+ */
+public class MartMain {
+    public static void main(String[] args) {
+        Login LoginDulu = new Login();
+        LoginDulu.setVisible(true);
+        LoginDulu.pack();
+        LoginDulu.setLocationRelativeTo(null);
+    }
+}
